b13e04d47e91b66a492349e3f4be302994d775ed
Nome <miranhaomi@gmail.com>
2023-10-18 14:56:08 -0300

    Mario-Less for CS50
    Resolução completa da atividade Mario-Less do CS50

---
+++ Week01/mario-less/mario-less.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int line = 0; line < height; line++)
+    {
+        for (int space = height - 1; space > line; space--)
+        {
+            printf(" ");
+        }
+
+        for (int block = 0; block <= line; block++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
